faec9c30e5de0a4235db0bb982ed3b7c0b22e109
Dave Dash <dd+github@davedash.com>
2011-11-03 14:06:55 -0700

    Add statsd + basic data, bug 698799

--- settings.py
+++ settings.py
@@ -251,7 +251,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-
+    'commonware.response.middleware.GraphiteMiddleware',
+    'commonware.response.middleware.GraphiteRequestTimingMiddleware',
     'commonware.middleware.FrameOptionsHeader',
 )
 
